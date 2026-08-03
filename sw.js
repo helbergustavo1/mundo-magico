cd87ad8a2a206c5ba0f883c687ae6303c589aec4
helbergustavo1 <helbergustavo@gmail.com>
2026-08-03 13:07:19 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE = 'mundo-magico-v1';
+const CORE = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(CORE)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys()
+    .then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))
+    .then(() => self.clients.claim()));
+});
+
+self.addEventListener('fetch', e => {
+  const req = e.request;
+  if (req.method !== 'GET') return;
+  e.respondWith(
+    caches.match(req, { ignoreSearch: true }).then(hit => {
+      if (hit) return hit;
+      return fetch(req).then(res => {
+        if (res && (res.ok || res.type === 'opaque')) {
+          const copy = res.clone();
+          caches.open(CACHE).then(c => c.put(req, copy));
+        }
+        return res;
+      }).catch(() => {
+        if (req.mode === 'navigate') return caches.match('./index.html');
+        return Response.error();
+      });
+    })
+  );
+});
