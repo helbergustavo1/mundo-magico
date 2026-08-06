6f22545084cf92371be2dca42ac4055142b1b044
helbergustavo1 <helbergustavo@gmail.com>
2026-08-06 18:43:17 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mundo-magico-v5';
+const CACHE = 'mundo-magico-v6';
 const CORE = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', e => {
